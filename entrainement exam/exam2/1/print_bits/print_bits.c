e564e59001ed395e65e5757949f5312c222173bc
Valentin Mologlou <vamologl@c1r2p7.42lehavre.fr>
2023-06-15 11:54:37 +0200

    brute force print_bits

--- entrainement exam/exam2/1/print_bits/print_bits.c
+++ entrainement exam/exam2/1/print_bits/print_bits.c
@@ -1,22 +1,81 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int	get_int(unsigned char s)
+
+{
+	int	i = s;
+	return (i);
+}
+
 void	print_bits(unsigned char octet)
 {
-	int	i = 7;
+	int	i = 0;
+	int	nb = get_int(octet);
 
-	while (i >= 0)
+	while (i <= 7)
 	{
-		if (octet & (1 << i))
+		if (i == 0 && nb - 128 >= 0)
+		{
+			nb = nb - 128;
 			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 1 && nb - 64 >= 0)
+		{
+			nb = nb - 64;
+			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 2 && nb - 32 >= 0)
+		{
+			nb = nb - 32;
+			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 3 && nb - 16 >= 0)
+		{
+			nb = nb - 16;
+			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 4 && nb - 8 >= 0)
+		{
+			nb = nb - 8;
+			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 5 && nb - 4 >= 0)
+		{
+			nb = nb - 4;
+			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 6 && nb - 2 >= 0)
+		{
+			nb = nb - 2;
+			write(1, "1", 1);
+			i++;
+		}
+		else if (i == 7 && nb - 1 >= 0)
+		{
+			nb = nb - 1;
+			write(1, "1", 1);
+			i++;
+		}
 		else
+		{
 			write(1, "0", 1);
-		i--;
+			i++;
+		}
 	}
 }
+
 /*
 int	main(void)
 {
-	print_bits(3);
+	char	c = 254;
+	printf("%d\n", get_int(c));
+	print_bits(c);
 	return (0);
 }*/
